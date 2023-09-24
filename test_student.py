f34f02c55d42939726e8fef61b8c7a0b4afc747c
b1ndark <>
2023-09-24 16:34:25 +0000

    add end date + extension function

--- test_student.py
+++ test_student.py
@@ -1,23 +1,44 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDopwnClass")
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_email(self):
-        student = Student("John", "Doe")
-
-        self.assertEqual(student.email, "john.doe@email.com")
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
     def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
+        print("test_alert_santa")
+        self.student.alert_santa()
 
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
 
 
 if __name__ == "__main__":
